87bbb129fff573b8e3e73c539ecb279c538c5af3
Kriss-Kross33 <kwamefosu33@gmail.com>
2025-05-13 13:52:36 +0000

    add NativeWind and TailwindCSS dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-// import React from 'react';
-// import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
-// import React, { useEffect } from 'react';
-// import {initGoogleSignIn} from "./services/socialAuthService";
-// import {MacroInputScreen} from "./screens/MacroInputScreen";
-//
-// export default function App() {
-//     useEffect(() => {
-//         // Initialize Google Sign In
-//         initGoogleSignIn();
-//     }, [])
-//
-//     return (
-//         <SafeAreaView style={styles.container} edges={['top', 'left', 'right', 'bottom']}>
-//             <StatusBar style="auto" />
-//             <MacroInputScreen />
-//         </SafeAreaView>
-//     );
-// }
-//
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1,
-//         backgroundColor: '#fff',
-//     },
-// });
+import './globals.css'
+import React, { useEffect } from 'react';
+import { StatusBar } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { initGoogleSignIn } from "./services/socialAuthService";
+import { MacroInputScreen } from "./screens/MacroInputScreen";
+
+export default function App() {
+    useEffect(() => {
+        // Initialize Google Sign In
+        initGoogleSignIn();
+    }, [])
+
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }} edges={['top', 'left', 'right', 'bottom']}>
+            <StatusBar barStyle="dark-content" />
+            <MacroInputScreen />
+        </SafeAreaView>
+    );
+}
