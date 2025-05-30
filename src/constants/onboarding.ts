5615904ff2ead63750aaa947fb444c15e7eca656
Kriss-Kross33 <kwamefosu33@gmail.com>
2025-05-30 09:59:41 +0000

    replace the onboarding dummy data with the actual data.

--- src/constants/onboarding.ts
+++ src/constants/onboarding.ts
@@ -8,23 +8,23 @@ export interface OnboardingItem {
 
 export const onboardingItems: OnboardingItem[] = [
     {
-        title: "Welcome to Macro Meals",
-        description: "Macro Meals is a mobile app that helps you track your macros and improve your health.",
+        title: "Macro calculator",
+        description: "Find your ideal daily macros fast. Get personalized targets for protein, carbs, and fat based on your age, weight, activity level, and goals no guesswork needed.",
         imagePath: IMAGE_CONSTANTS.macroCalculator,
     },
     {
-        title: "Track your macros",
-        description: "Macro Meals is a mobile app that helps you track your macros and improve your health.",
+        title: "Macro dashboard",
+        description: "The Macro Dashboard shows your calories, protein, carbs, and fat in a clear visual breakdown so you know exactly what you’ve consumed, what’s left, and how close you are to your goals.",
         imagePath: IMAGE_CONSTANTS.macroDashboard,
     },
     {
-        title: "Track your macros",
-        description: "Macro Meals is a mobile app that helps you track your macros and improve your health.",
+        title: "Nearby meal finder",
+        description: "Find meals that fit your goals wherever you are. The Location-Based Meal Finder surfaces nearby restaurant options that match your macros, so you can eat out or order in without compromising your progress.",
         imagePath: IMAGE_CONSTANTS.nearbyMealFinder,
     },
     {
-        title: "Track your macros",
-        description: "Macro Meals is a mobile app that helps you track your macros and improve your health.",
+        title: "AI meal suggestions",
+        description: "Hit your goals with smart meal suggestions. Our AI analyzes your remaining macros and recommends foods that help you meet your daily targets making it easier to stay on track with every bite.",
         imagePath: IMAGE_CONSTANTS.aiSuggestedMeals,
     },
 ]  
