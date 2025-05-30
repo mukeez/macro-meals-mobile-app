e3193e9f9eda3f57f76c59c999439797758a3be9
Kriss-Kross33 <kwamefosu33@gmail.com>
2025-05-30 10:01:08 +0000

    increase border radius of CustomTouchableOpacityButton

--- src/components/CustomTouchableOpacityButton.tsx
+++ src/components/CustomTouchableOpacityButton.tsx
@@ -16,7 +16,7 @@ export default function CustomTouchableOpacityButton({
     textClassName?: string;
 }){
     return (
-        <TouchableOpacity onPress={onPress} disabled={disabled} className={className ?? 'h-[56px] bg-primary rounded-[100px] p-4'} >
+        <TouchableOpacity onPress={onPress} disabled={disabled} className={className ?? 'h-[56px] items-center justify-center bg-primary rounded-[100px]'} >
             <Text className={textClassName ?? 'text-white text-center text-base font-semibold'}>{title}</Text>
         </TouchableOpacity>
     )
