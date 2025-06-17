c827d7544cf328cf3dfdf00ba5e69edcc04ff021
Kriss-Kross33 <kwamefosu33@gmail.com>
2025-06-17 14:11:53 +0000

    redesign personalizsed plan to match figma design

--- src/components/goal_flow_components/your_plan/GoalsPersonalizedPlan.tsx
+++ src/components/goal_flow_components/your_plan/GoalsPersonalizedPlan.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, ActivityIndicator } from 'react-native'
 import BackButton from 'src/components/BackButton'
 import { MacroCircle } from 'src/components/MacroCircle'
 
@@ -20,33 +20,48 @@ const SegmentedProgressBar = ({ current, total }: { current: number; total: numb
   </View>
 )
 
-export const GoalsPersonalizedPlan: React.FC = () => {
+export const GoalsPersonalizedPlan: React.FC<{ isLoading: boolean, macroData: any, calorieTarget?: number }> = ({ isLoading, macroData, calorieTarget }) => {
   return (
-    <View className="flex-1 bg-white px-4 pt-2">
+    
+    <View className="flex-1 bg-white pt-2">
       {/* Header */}
       {/* Title & Description */}
+      {isLoading ? (
+        <View className="flex-1 items-center justify-center bg-white px-4 pt-2">
+          <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+      ) : (
+      <View>
+    
       <Text className="text-3xl font-bold mt-2 mb-2">Your personalized plan</Text>
       <Text className="text-base text-gray-500 mb-6">Based on your information, we've calculated your ideal daily macronutrient targets to help you reach your goals</Text>
+      {/* Calorie Target */}
+      {calorieTarget !== undefined && (
+        <Text className="text-lg font-bold mb-2 text-primary">Daily Calories: {calorieTarget} kcal</Text>
+      )}
       {/* Macro Targets */}
-      <Text className="text-lg font-semibold mb-2">Daily macro targets:</Text>
-      <View className="flex-row items-center justify-between mb-6">
-        {macroData.map((macro) => (
+      <Text className="text-lg font-semibold mb-8">Daily macro targets:</Text>
+      <View className="flex-row items-center mx-6 justify-between mb-6">
+        {macroData.map((macro: any) => (
           <MacroCircle
             size={100}
             key={macro.type}
             type={macro.type}
             value={macro.value}
             progress={100}
+            strokeWidth={3}
             color={macro.color}
           />
         ))}
       </View>
       {/* Info Text */}
-      <Text className="text-base text-gray-500 mb-2">
+      <Text className="text-base text-gray-500 mb-2 leading-6 tracking-widest">
         These targets are specifically designed to support your fitness journey. Following this nutrition plan, you will <Text className="text-primary font-semibold">[lose/gain] [XX] kg</Text> by <Text className="text-primary font-semibold">[Month]</Text>.
       </Text>
-      <Text className="text-base text-gray-500 mb-8">Ready to start tracking your personalized plan?</Text>
+      <Text className="text-base text-gray-500 mb-8 mt-4 leading-6 tracking-widest">Ready to start tracking your personalized plan?</Text>
       {/* Confirm Button */}
+      </View>
+      )}
     </View>
   )
 }
