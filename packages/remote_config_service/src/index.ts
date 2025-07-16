39abf01c8c0a50f7b591f5cfbfb042927244e605
Kriss-Kross33 <veritaslogisticsdev@gmail.com>
2025-07-16 18:08:30 +0000

    integrate firebase remote config to bypass paywall for testers

---
+++ packages/remote_config_service/src/index.ts
@@ -0,0 +1,9 @@
+// Main service
+export { default as RemoteConfigService } from './remote_config_service';
+
+// React components and hooks
+export { RemoteConfigProvider, useRemoteConfigContext } from './RemoteConfigProvider';
+export { useRemoteConfig } from './hooks/useRemoteConfig';
+
+// Types
+export * from './types';
