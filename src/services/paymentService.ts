e3c8a313c695837b83c6541e8c96926a943a25a0
Kriss-Kross33 <veritaslogisticsdev@gmail.com>
2025-07-16 17:53:14 +0000

    feat: use webview to render stripe payment flow

--- src/services/paymentService.ts
+++ src/services/paymentService.ts
@@ -15,18 +15,34 @@ export const paymentService = {
         }
     },
 
+    
+
     // createSetupIntent: async () => {
     //     const response = await axiosInstance.post('/billing/create-setup-intent');
     //     return response.data;
     // },
 
+    checkout: async (email: string, plan: string, userId: string) => {
+        try {
+            const response = await axiosInstance.post('/billing/checkout', {
+                email,
+                plan,
+                user_id: userId
+            });
+            return response.data;
+        } catch (error) {
+            console.error('Checkout error:', error);
+            throw error;
+        }
+    },
+
     createPaymentIntent: async (email: string, user_id: string, plan: string) => {
         try {
             const response = await axiosInstance.post('/billing/create-setup-intent', {
-                email,
-                user_id,
-                plan
-            });
+                    email,
+                    user_id,
+                    plan 
+                });
 
             console.log('Payment intent response:', response.data);
             return response.data;
